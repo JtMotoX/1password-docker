151ba8c4db71b09bfd899d85899ef618d0d7b289
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2022-04-25 22:03:25 -0700

    entrypoint init now throws error if unable to get value

--- 1password/op-vars.sh
+++ 1password/op-vars.sh
@@ -14,6 +14,10 @@ while IFS= read -r line; do
 		vault_name=$(echo "${val}" | sed -E "s/${regexp}/\1/")
 		vault_item_title=$(echo "${val}" | sed -E "s/${regexp}/\2/")
 		vault_item_value=$(curl -s -k -X POST -H "content-type: application/json" -d "{\"vault_name\": \"${vault_name}\", \"item_title\": \"${vault_item_title}\", \"auth\": \"${OP_AUTH}\"}" "${OP_ENDPOINT}/get-password")
+		if echo "${vault_item_value}" | grep -E '^ERROR: ' >/dev/null; then
+			echo "Unable to get 1password value for '${key}' (${vault_item_value})"
+			exit 1
+		fi
 		export ${key}="${vault_item_value}"
 	fi
 done < /tmp/env.txt
