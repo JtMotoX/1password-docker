c91d221406802a140fd6cd01ff14ab90660d9767
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2022-06-20 00:02:11 -0700

    add error output

--- sample-app/entrypoint.sh
+++ sample-app/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # REQUIRED FOR METHOD 1
-curl -s -o /tmp/1password-vars.sh "https://raw.githubusercontent.com/JtMotoX/1password-docker/main/1password/op-vars.sh"
+curl -sS -o /tmp/1password-vars.sh "https://raw.githubusercontent.com/JtMotoX/1password-docker/main/1password/op-vars.sh"
 chmod 755 /tmp/1password-vars.sh
 . /tmp/1password-vars.sh || exit 1
 rm -f /tmp/1password-vars.sh
