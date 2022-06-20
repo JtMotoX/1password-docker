04885a69c293d9093c563357589968806f57319e
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2022-06-19 23:55:16 -0700

    fix if app has set -e

--- 1password/op-vars.sh
+++ 1password/op-vars.sh
@@ -13,13 +13,13 @@ while IFS= read -r line; do
 	if echo "${val}" | grep -E "${regexp}" >/dev/null; then
 		vault_name=$(echo "${val}" | sed -E "s/${regexp}/\1/")
 		vault_item_title=$(echo "${val}" | sed -E "s/${regexp}/\2/")
-		response=$(curl -sS -k -X POST -H "content-type: application/json" -d "{\"vault_name\": \"${vault_name}\", \"item_title\": \"${vault_item_title}\", \"auth\": \"${OP_AUTH}\"}" "${OP_ENDPOINT}/get-password")
-		RESPONSE_ERROR="$(echo "${response}" | grep -o '"error":"[^"]*' | grep -o '[^"]*$')"
+		response=$(curl -sS -k -X POST -H "content-type: application/json" -d "{\"vault_name\": \"${vault_name}\", \"item_title\": \"${vault_item_title}\", \"auth\": \"${OP_AUTH}\"}" "${OP_ENDPOINT}/get-password" || echo "")
+		RESPONSE_ERROR="$(echo "${response}" | grep -o '"error":"[^"]*' | grep -o '[^"]*$' || echo "")"
 		if [ "${RESPONSE_ERROR}" != "" ]; then
 			echo "ERROR: ${RESPONSE_ERROR}"
 			exit 1
 		fi
-		RESPONSE_PASSWORD="$(echo "${response}" | grep -o '"password":"[^"]*' | grep -o '[^"]*$')"
+		RESPONSE_PASSWORD="$(echo "${response}" | grep -o '"password":"[^"]*' | grep -o '[^"]*$' || echo "")"
 		if [ "${RESPONSE_PASSWORD}" = "" ]; then
 			echo "ERROR: Unable to get 1Password value for '${key}'"
 			exit 1
